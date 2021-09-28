17a3737256756fdb5f2725361665aede047e8479
Parris Greaves <parris.greaves@gmail.com>
2021-09-28 13:52:50 -0400

    working on the functionality to add a new URL.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
 app.set('view engine', "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  return Math.random().toString(36).slice(2, 8);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,12 +29,13 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//REMOVE??? This specific code block may just have been for example.
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
 });
 
-//REMOVE??? This code may just have been for example.
+//REMOVE??? This specific code block may just have been for example.
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
@@ -38,12 +45,24 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
+// Adding a new Tiny URL
+// Creating the form for the new url submission
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+// Posting the information from the form
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+
+app.get("/urls/:id", (req, res) => {
+  const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
   const templateVars = { shortURL, longURL };
   res.render("urls_show", templateVars);
-})
+});
 
 
 

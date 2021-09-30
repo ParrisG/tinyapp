5b4bc2fd60b55b0f62ce3d1faa3dd04aa39d4e44
Parris Greaves <parris.greaves@gmail.com>
2021-09-30 18:49:52 -0400

    adjusted the logout endpoint to remove the session cookies

--- express_server.js
+++ express_server.js
@@ -15,37 +15,6 @@ app.use(cookieSession({
 }));
 
 
-// //Helper Functions
-
-// //This function produces a random string (used for creating unique Ids)
-// const generateRandomString = () => {
-//   return Math.random().toString(36).slice(2, 8);
-// };
-
-// //This function checks to see if a provided email is in the users db. It returns the entire user object if found, or false.
-// const findUserByEmail = (email, users) => {
-//   for (let user in users) {
-//     if (users[user].email.toLowerCase() === email.toLowerCase()) {
-//       return users[user];
-//     }
-//   }
-//   return false;
-// };
-
-// //This function loops through the urlDatabase and returns all the records (as a new db type object) belonging to a specific user.
-// const filterUrlDatabaseByUser = (user_id, database) => {
-//   const ownedUrlDatabase = {};
-//   for (let record in database) {
-//     if (database[record].userID === user_id) {
-//       ownedUrlDatabase[record] = database[record];
-//     }
-//   }
-//   return ownedUrlDatabase;
-  
-// }
-
-//"Database" objects for use in the project
-
 const users = {
   "userRandomID": {
     id: "userRandomID",
@@ -235,9 +204,10 @@ app.post("/login", (req, res) => {
   res.redirect('/urls');
 });
 
-// Endpoint for the user to signout. Clears the user_id cookie.
+
+// LOG-OUT - Endpoint for the user to signout. Clears the user_id cookie.
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect("/urls");
 });
 
